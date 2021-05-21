34e5b9c4e7c61e6f38bdf1ff5bf48311949c5742
Faisal-ZaheerVI <faisalszaheer@gmail.com>
2021-05-20 17:49:45 -0700

    Update Nokia5110.h

--- Nokia5110.h
+++ Nokia5110.h
@@ -1,9 +1,6 @@
 // Nokia5110.h
-// Runs on LM4F120/TM4C123
 // Use SSI0 to send an 8-bit code to the Nokia5110 48x84
 // pixel LCD to display text, images, or other information.
-// Daniel Valvano
-// September 16, 2013
 
 // Font table, initialization, and other functions based
 // off of Nokia_5110_Example from Spark Fun:
@@ -26,18 +23,6 @@
  For more information about my classes, my research, and my books, see
  http://users.ece.utexas.edu/~valvano/
  */
-
-// Blue Nokia 5110
-// ---------------
-// Signal        (Nokia 5110) LaunchPad pin
-// Reset         (RST, pin 1) connected to PA7
-// SSI0Fss       (CE,  pin 2) connected to PA3
-// Data/Command  (DC,  pin 3) connected to PA6
-// SSI0Tx        (Din, pin 4) connected to PA5
-// SSI0Clk       (Clk, pin 5) connected to PA2
-// 3.3V          (Vcc, pin 6) power
-// back light    (BL,  pin 7) not connected
-// Ground        (Gnd, pin 8) ground
 
 // Red SparkFun Nokia 5110 (LCD-10168)
 // -----------------------------------
